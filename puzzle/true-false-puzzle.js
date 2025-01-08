e33fda9f65baf3419380b90ad8dc689d71319274
ilovehostingwmcsites <flamingdelsolm@gmail.com>
2025-01-08 00:27:30 -0500

    Update true-false-puzzle.js
    pains

--- puzzle/true-false-puzzle.js
+++ puzzle/true-false-puzzle.js
@@ -3,8 +3,11 @@ function checkAnswer(isTrue) {
   const errorMessage = document.getElementById('error-message');
 
   if (isTrue === correctAnswer) {
-    document.getElementById('true-false-container').style.display = 'none';
-    document.getElementById('next-clue').style.display = 'block';
+    document.body.innerHTML = `
+      <h1>Great job on making it this far!</h1>
+      <img src="WMCPortfolio1.png" alt="Portfolio 1">
+      <button onclick="goBackToHome()">Back to Home</button>
+    `;
   } else {
     errorMessage.textContent = 'Incorrect answer. Please try again.';
   }
