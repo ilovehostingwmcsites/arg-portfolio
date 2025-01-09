0fbaf39641fc4ee356827409ec159c2ceb0d8cfb
ilovehostingwmcsites <flamingdelsolm@gmail.com>
2025-01-08 23:33:35 -0500

    Update returnofscript2.js

--- puzzle/returnofscript2.js
+++ puzzle/returnofscript2.js
@@ -8,7 +8,7 @@ function checkRiddleAnswer() {
     // Display the next clue (portfolio images)
     document.body.innerHTML = `
       <h1>Well done! You've solved the puzzle.</h1>
-      <img src="WMCPortfolio1.png" alt="Portfolio 1">
+      <img src="WMCPortfolio3.png" alt="Portfolio 3">
       <button onclick="goBackToHome()">Back to Home</button>
     `;
   } else {
